652fa9cf076fa7824b06e5f224ec12959a557d00
Alannah Kearney <hi@alannahkearney.com>
2020-11-22 09:22:14 +1000

    fix: Use correct namesapce

--- src/Extended/Interfaces/RouteInterface.php
+++ src/Extended/Interfaces/RouteInterface.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace pointybeard\Symphony\Extended;
+namespace pointybeard\Symphony\Extended\Interfaces;
 
 use Symfony\Component\HttpFoundation;
 
